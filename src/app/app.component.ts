32b456c063a6a8e8135e990c96bbea007937611e
黃郁倫 mio <mio@miniasp.com>
2019-08-26 18:18:44 +0800

    part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,15 @@ export class AppComponent {
   title = 'app';
   url = 'http://blog.miniasp.com/';
   imgurl = '/assets/images/logo.png';
+  words = 0;
   constructor() {
   }
   changeTitle(altKey: boolean) {
     if (altKey) {
-      this.title  = 'The Will Will Web';
+      this.title = 'The Will Will Web';
     }
+  }
+  ChangeSearsh($event: KeyboardEvent) {
+    this.words = $event.target.value.length;
   }
 }
